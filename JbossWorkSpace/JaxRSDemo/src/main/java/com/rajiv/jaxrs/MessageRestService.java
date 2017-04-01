ca565984903ca5d14d328da72ce01fb47ddab65f
Rajiv <rajeksingh2@gmail.com>
2017-04-01 12:46:21 +0530

    Jax RS Demo

---
+++ JbossWorkSpace/JaxRSDemo/src/main/java/com/rajiv/jaxrs/MessageRestService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.rajiv.jaxrs;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+/**
+ * @author Rajiv
+ *
+ */
+@Path("/message")
+public class MessageRestService {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+}
